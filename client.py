b3e9b0b2164a0bf6923b4e68534657b50f5ea7bd
Ludwik Rekowski <n4o966@gmail.com>
2017-01-30 11:45:05 +0100

    Update client.py
    added connect func

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
 #!usr/bin/python
 
+import socket
+
 class Connection:
         def __init__(self, path):
                 self.path = path
                 f = open(self.path, 'r')
                 self.file =  f.readlines()
                 self.peers = []
+                self.sockets = []
+                
         def getPeers(self):
                 for line in self.file:
                         print line
@@ -14,7 +18,16 @@ class Connection:
                         line[1] = int(line[1])
                         self.peers.append(line)
                 return self.peers
-                
+        
+        def connect(self):
+                i = 0
+                for a in self.peers:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect(tuple(a))
+                        self.sockets[i] = s
+                        i += 1
+                return self.sockets
+        
 test = Connection("test.txt")
-test.getPeers()
-print test.peers
+print test.getPeers()
+test.connect()
