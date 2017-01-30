0cc1dc620c26e4b9ab2f6191685dd709b43c1766
Ludwik Rekowski <n4o966@gmail.com>
2017-01-30 11:49:47 +0100

    Update client.py
    added timeout

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+
 #!usr/bin/python
 
 import socket
@@ -9,7 +10,6 @@ class Connection:
                 self.file =  f.readlines()
                 self.peers = []
                 self.sockets = []
-                
         def getPeers(self):
                 for line in self.file:
                         print line
@@ -18,19 +18,19 @@ class Connection:
                         line[1] = int(line[1])
                         self.peers.append(line)
                 return self.peers
-        
         def connect(self):
                 i = 0
                 for a in self.peers:
                         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.settimeout(0.5)
                         try:
                                 s.connect(tuple(a))
                         except:
                                 print "Cannot connect to: " + str(a[0])
-                        self.sockets[i] = s
+                        self.sockets.append(s)
                         i += 1
                 return self.sockets
-        
 test = Connection("test.txt")
-print test.getPeers()
-test.connect()
+test.getPeers()
+print test.connect()
+
