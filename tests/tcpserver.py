feecfe40e50e02d5b308404343eb5314e3c55ab1
Ludwik Rekowski <n4o966@gmail.com>
2017-02-01 21:13:28 +0100

    Update tcpserver.py

--- tests/tcpserver.py
+++ tests/tcpserver.py
@@ -1,1 +1,15 @@
 #!usr/bin/python
+import socket
+
+HOST = '192.168.1.2'
+PORT = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+
+s.listen(1)
+
+while 1:
+  conn, addr = s.accept()
+  data = conn.recv(1024)
+  print data
